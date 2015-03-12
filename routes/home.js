73b2f56065706648904e51beff875f01db8fa498
William Werner <wwerner@ucsd.edu>
2015-03-12 05:11:41 +0000

    Fixed layout of ordering and removed profile

--- routes/home.js
+++ routes/home.js
@@ -2,12 +2,5 @@
 var data = require('../data.json');
 
 exports.view = function(req, res){
-	var random_num = Math.random();
-
-	if(random_num > 0.5){
-    	res.render("home", data);
-    }else{
-    	res.render("althome", data)
-  }
 	res.render('home', data);
 };
